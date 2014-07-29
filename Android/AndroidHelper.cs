c98400e40e435f2d6be74af6d7c251a9dee64069
Andrew Didenko <devolap@gmail.com>
2014-07-29 13:46:31 +0600

    init

---
+++ Android/AndroidHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class AndroidHelper {
+    public static string GetInstallerId(string packageName) {
+#if UNITY_ANDROID
+        var installerId = "";
+        try {
+            AndroidJNI.AttachCurrentThread();
+            using (var jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
+                using (var curActivity = jc.GetStatic<AndroidJavaObject>("currentActivity")) {
+                    using (var pm = curActivity.Call<AndroidJavaObject>("getPackageManager")) {
+                        installerId = pm.Call<string>("getInstallerPackageName", new object[] { packageName });
+                    }
+                }
+            }
+
+        } catch (Exception ex) {
+            Debug.Log(ex.Message);
+        }
+        return installerId;
+#else
+        return "";
+#endif
+    }
+}
